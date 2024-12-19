7697b4ef9598ae9a203469629d01f97d14a16ed1
IJLAL <ijlal.tanveer294@gmail.com>
2024-12-19 20:36:16 +0500

    added code for render deployment

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import '../styles/Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>© 2024 Fashion Store. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
